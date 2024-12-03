d0bf48cd446cc50ae711e1d528f2d413499d90fe
Jo Giraerts <jo@rombit.be>
2024-12-03 10:15:51 +0100

    footer fixed to bottom

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,4 @@
-<div id="footer" class="well well-sm bg-info clearfix">
+<div id="footer" class="well well-sm bg-info clearfix navbar-fixed-bottom">
     <b></b>
     <span>This site does not use cookies. All data entered by users will be deleted 24h after last modification.</span>
     <span class="pull-right">© <?= date("Y")?> <a href="mailto:jo.giraerts@gmail.com">Jo Giraerts</a></span>
